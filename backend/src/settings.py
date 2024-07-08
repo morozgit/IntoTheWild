7aefccda60f9ec0a8098b2544d4eeacffa765f09
morozgit <69896562+morozgit@users.noreply.github.com>
2024-07-08 20:13:09 +0300

    add choice platform

--- backend/src/settings.py
+++ backend/src/settings.py
@@ -1,8 +1,15 @@
 from envparse import Env
+import platform
 
 env = Env()
 
-DATABASE_URL = env.str(
-    "DATABASE_URL",
-    default="postgresql+asyncpg://postgres:postgres@127.0.0.1:5432/postgres",
-)
+if platform.system() == "Linux":
+    DATABASE_URL = env.str(
+        "DATABASE_URL",
+        default="postgresql+asyncpg://postgres:postgres@127.0.0.1:5432/postgres",
+    )
+elif platform.system() == "Windows":
+    DATABASE_URL = env.str(
+        "DATABASE_URL",
+        default="postgresql+asyncpg://postgres:postgres@127.0.0.1:5433/postgres",
+    )
